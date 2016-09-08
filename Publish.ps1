e446d04e6ab1d3a2c10b405bdfd01bb95423c656
Stephane Erbrech <stephane.erbrech@if.no>
2016-09-08 14:16:16 +0200

    bump version

--- Publish.ps1
+++ Publish.ps1
@@ -1,10 +1,14 @@
 param(
-    [string]$apiKey
+    [string]$apiKey,
+    [string]$version
 )
+if(!$version){
+    throw "set a version carefully"
+}
 
 $modulepath = Resolve-Path "$PSScriptRoot/DSCResources/cRavenDB"
 if(-not ($Env:PSModulePath.Split(";") -contains $modulepath)){
     $Env:PSModulePath = "$modulepath;$Env:PSModulePath"
 }
 
-Publish-Module -Name cRavenDB -NuGetApiKey $apiKey
+Publish-Module -Name cRavenDB -NuGetApiKey $apiKey -RequiredVersion $version
